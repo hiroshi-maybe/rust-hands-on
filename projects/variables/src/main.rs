c330f67e77bc37feb6b89f3abeb353a33ca3f3f2
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-07-29 23:19:47 -0700

    Add variables project in Chapter 3
    https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html

---
+++ projects/variables/src/main.rs
@@ -0,0 +1,17 @@
+// https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html
+
+fn main() {
+    let mut x=5;
+    println!("The value of x is: {}", x);
+    x=6;
+    println!("The value of x is: {}", x);
+
+    let x=5;
+    let x=x+1;
+    let x=x*2;
+    println!("The value of x is: {}", x);
+
+    let spaces="   ";
+    let spaces=spaces.len();
+    println!("Number of spaces is: {}", spaces)
+}
