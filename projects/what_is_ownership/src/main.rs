9d0374e96669e9eb63fb7823978fab346752b4eb
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-08-06 23:31:14 -0700

    Complete Ch 4-1 What Is Ownership?

--- projects/what_is_ownership/src/main.rs
+++ projects/what_is_ownership/src/main.rs
@@ -53,5 +53,74 @@ fn main() {
         let s4 = s3.clone();
         s3.push_str(", world!");
         println!("s3={}, s4={}", s3, s4);
+
+        // String literal has a fixed size. Thus it's stored in stack.
+        // Since they are copied, compiler never throws an error.
+        let s5 = "hello";
+        let s6 = s5;
+        println!("s5={}, copied s6={}", s5, s6);
+    }
+
+    {
+        // Ownership and Functions
+
+        let s = String::from("hello");
+        println!("before moving to a function: {}", s);
+        takes_ownership(s);
+        // Ownership is moved to the `takes_ownership()`.
+        // Compile-time error!
+        // println!("{}", s);
+
+        fn takes_ownership(s: String) {
+            println!("moved to a function: {}", s);
+        }
+
+        let n = 5;
+        makes_copy(n);
+        // No Compile error because `n` is copied to `makes_copy()`
+        println!("Copied value is still valid: {}", n);
+
+        fn makes_copy(n: i32) {
+            println!("Copied to a function: {}", n);
+        }
+    }
+
+    {
+        // Return Values and Scope
+
+        let s1 = gives_ownership();
+
+        fn gives_ownership() -> String {
+            String::from("hello")
+        }
+
+        println!("given string along with ownership: {}", s1);
+
+        let s2 = takes_and_gives_back(s1);
+        // Compile-time error because s1 is moved to the function
+        // println!("{}", s1);
+        println!("given string along with ownership: {}", s2);
+
+        fn takes_and_gives_back(s: String) -> String { s }
+    }
+
+    {
+        // Reuse data by returning from a function with ownership
+        let s1 = String::from("hello");
+        let (s2, len) = calc_length(s1);
+        println!("{} is returned back from a function with length {}", s2, len);
+
+        fn calc_length(s: String) -> (String, usize) {
+            // This gives compile-time error because s is moved in the first item of the tuple
+            //(s, s.len())
+
+            let l = s.len();
+            (s, l)
+        }
+
+        let (len, s1) = calc_length2(s2);
+        println!("{} is returned back from a function with length {}", s1, len);
+
+        fn calc_length2(s: String) -> (usize, String) { (s.len(), s) }
     }
 }
