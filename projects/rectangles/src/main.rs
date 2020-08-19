450e08d829e8231e2e741d47c5449c12bfd1ad04
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-08-18 23:03:30 -0700

    Complete Ch 5-3 Method Syntax

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -17,7 +17,18 @@ fn main() {
         height: u32,
     }
 
+    impl Rectangle {
+        fn area(&self) -> u64 {
+            (self.width as u64) * (self.height as u64)
+        }
+
+        fn can_hold(&self, other: &Rectangle) -> bool {
+            self.width >= other.width && self.height >= other.height
+        }
+    }
+
     {
+        // Area
         let rect1 = Rectangle {
             width: w,
             height: h,
@@ -30,5 +41,61 @@ fn main() {
 
         println!("area for {:?}: {}", rect1, area(&rect1));
         println!("area for pretty {:#?}: {}", rect1, area(&rect1));
+        println!("area calculated through a method: {}", rect1.area());
+    }
+
+    {
+        // Hold
+        let rect1 = Rectangle {
+            width: 30,
+            height: 50,
+        };
+        let rect2 = Rectangle {
+            width: 10,
+            height: 40,
+        };
+        let rect3 = Rectangle {
+            width: 60,
+            height: 45,
+        };
+
+        let rects = [rect1, rect2, rect3];
+        for j in 0..rects.len() {
+            for i in 0..rects.len() {
+                if i != j {
+                    println!("Can rect{} {:?} hold rect{} {:?}? {}",
+                    i+1, rects[i], j+1, rects[j],
+                    rects[i].can_hold(&rects[j]));
+                }
+            }
+        }
+    }
+
+    {
+        // Associated functions
+
+        impl Rectangle {
+           fn square(size: u32) -> Rectangle {
+                Rectangle {
+                    width: size,
+                    height: size,
+                }
+            }
+        }
+
+        let rect1 = Rectangle {
+            width: 30,
+            height: 50,
+        };
+
+        let sq = Rectangle::square(20);
+        println!("Rect {:?} can hold square {:?}? {}",
+        rect1, sq, rect1.can_hold(&sq));
+    }
+
+    {
+        // `impl` blocks are visible outside of the scope
+        let x = Rectangle::square(10);
+        println!("{:?}", x);
     }
 }
