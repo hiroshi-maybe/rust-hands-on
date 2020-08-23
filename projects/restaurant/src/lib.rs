83e28170b20f5ccab2b4a61d9c94c2e8483da9d8
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-08-23 16:14:49 -0700

    Complete Ch 7-3 Paths for Referring to an Item in the Module Tree

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -1,6 +1,8 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {
+            println!("Added to a wait list");
+        }
         fn seat_at_table() {}
     }
 
@@ -9,4 +11,36 @@ mod front_of_house {
         fn serve_order() {}
         fn take_payment() {}
     }
+}
+
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
 }
