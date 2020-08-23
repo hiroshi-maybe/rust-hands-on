0c6f470a158d212738d6b923a9329a0a20ebe1f1
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-08-23 16:47:16 -0700

    Complete Ch 7-4 Bringing Paths into Scope with the use Keyword

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -34,9 +34,11 @@ mod back_of_house {
     }
 }
 
+pub use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
-    front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     let mut meal = back_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Wheat");
@@ -44,3 +46,6 @@ pub fn eat_at_restaurant() {
 
     let order1 = back_of_house::Appetizer::Soup;
 }
+
+use std::{cmp::Ordering, io};
+use std::collections::*;
