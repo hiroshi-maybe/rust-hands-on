8c0b79a05b5ab38a98b74c534140bcdd07735849
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-09-26 23:23:43 -0700

    Finish Writing a Failing Test in Ch 12-4

--- projects/minigrep/src/lib.rs
+++ projects/minigrep/src/lib.rs
@@ -3,9 +3,12 @@ use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n {}", contents);
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
 }
 
 pub struct Config {
@@ -23,5 +26,21 @@ impl Config{
         let filename = args[2].clone();
 
         Ok(Config { query, filename })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
