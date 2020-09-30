4283abe8515876695cbee0636af316f2a3b11549
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-09-29 22:48:27 -0700

    Complete Ch 13-1 Closures: Anonymous Functions that Can Capture Their Environment

--- projects/closures/src/main.rs
+++ projects/closures/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 
@@ -8,21 +9,55 @@ fn main() {
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
+struct Cacher<T>
+where T: Fn(u32) -> u32,
+{
+    calculation: T,
+    memo: HashMap<u32, u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            memo: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        let v = self.memo.entry(arg).or_insert((self.calculation)(arg));
+        *v
+    }
+}
+
+#[test]
+fn call_with_different_values() {
+    let mut c = Cacher::new(|a| a);
+
+    let _v1 = c.value(1);
+    let v2 = c.value(2);
+
+    assert_eq!(v2, 2);
+}
+
 fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num: u32| -> u32 {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
     if intensity < 25 {
-        println!("Today, do {} pushups!", simulated_expensive_calculation(intensity));
-        println!("Next, do {} situps!", simulated_expensive_calculation(intensity));
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!("Today, run for {} minutes!", simulated_expensive_calculation(intensity));
+            println!("Today, run for {} minutes!", expensive_result.value(intensity));
         }
     }
-}
-
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
 }
