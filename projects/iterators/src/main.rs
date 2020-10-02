4755bcc98f9a74b8396712e27e2338304f34ee3a
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-10-01 21:56:32 -0700

    Complete 13-2 Processing a Series of Items with Iterators

--- projects/iterators/src/main.rs
+++ projects/iterators/src/main.rs
@@ -10,6 +10,13 @@ fn main() {
         // This does not work because it is moved in `for` loop
         // it.next();
     }
+
+    {
+        let v1: Vec<i32> = vec![1,2,3];
+        let v2 = v1.iter().map(|&x| x + 1).collect::<Vec<_>>();
+        //let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+        assert_eq!(v2, vec![2,3,4]);
+    }
 }
 
 #[test]
