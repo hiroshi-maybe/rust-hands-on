e29cf142e5930b3cd6b06ddb8723388e77b857a1
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-10-06 21:58:29 -0700

    Complete Ch 15-1 Using Box<T> to Point to Data on the Heap

--- projects/boxes/src/main.rs
+++ projects/boxes/src/main.rs
@@ -1,4 +1,20 @@
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+use crate::List::{Cons, Nil};
+
 fn main() {
-    let b = Box::new(5);
-    println!("b = {}", b);
+    {
+        // Using a Box<T> to Store Data on the Heap
+        let b = Box::new(5);
+        println!("b = {}", b);
+    }
+
+    {
+        // Enabling Recursive Types with Boxes
+        let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+        println!("{:?}", list);
+    }
 }
