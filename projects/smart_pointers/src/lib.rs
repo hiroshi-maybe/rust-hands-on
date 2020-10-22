cde01a15c92536019427053744692eb0498bb2eb
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-10-21 23:02:54 -0700

    Finish Keeping Track of Borrows at Runtime with RefCell<T> in h 15-5

--- projects/smart_pointers/src/lib.rs
+++ projects/smart_pointers/src/lib.rs
@@ -14,7 +14,7 @@ where
 {
     pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
         LimitTracker {
-            mesenger,
+            messenger,
             value: 0,
             max,
         }
@@ -34,5 +34,42 @@ where
             self.messenger
                 .send("Warning: You've used up over 75% of your quota!");
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+
+            // Runtime error: `already borrowed: BorrowMutError`
+            // let mut borrow1 = self.sent_messages.borrow_mut();
+            // let mut borrow2 = self.sent_messages.borrow_mut();
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
     }
 }
