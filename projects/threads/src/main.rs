5b0b7390176f2089c5136d7ece3753b8dc6f30a4
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-10-27 10:27:55 -0700

    Complete Ch 16-1 Using Threads to Run Code Simultaneously

--- projects/threads/src/main.rs
+++ projects/threads/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     {
         // Creating a New Thread with spawn
 
-        thread::spawn(|| {
+        let handle = thread::spawn(|| {
             for i in 1..10 {
                 println!("hi number {} from the spawned thread!", i);
                 thread::sleep(Duration::from_millis(1));
@@ -14,7 +14,25 @@ fn main() {
 
         for i in 1..5 {
             println!("hi number {} from the main thread!", i);
-            thread::sleep(Duration::from_millis(3));
+            thread::sleep(Duration::from_millis(1));
         }
+
+        handle.join().unwrap();
+
+        println!("The spawned thread joined");
+    }
+
+    {
+        // Using move Closures with Threads
+
+        let v = vec![1, 2, 3];
+        let handle = thread::spawn(move || {
+            println!("Here's a moved vector in a new thread: {:?}", v);
+        });
+
+        // Throws an error because `v` is moved above
+        // println!("{:?}", v);
+
+        handle.join().unwrap();
     }
 }
