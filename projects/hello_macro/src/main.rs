bce8a36bc49f3cf6e4bd7ccc3c099c726a2bff28
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-01-04 10:13:17 -0800

    Complete Ch 19-5. Macros
    Finish Attribute-like macros in Ch 19-5

--- projects/hello_macro/src/main.rs
+++ projects/hello_macro/src/main.rs
@@ -1,9 +1,15 @@
 use hello_macro::myvec;
 use hello_macro::HelloMacro;
 use hello_macro_derive::HelloMacro;
+use hello_macro_attribute::show_streams;
 
 #[derive(HelloMacro)]
 struct Pancakes;
+
+#[show_streams("attr message")]
+fn hello_attr_like_macro() {
+    println!("`hello_attr_like_macro` function called!");
+}
 
 fn main() {
     {
@@ -15,5 +21,10 @@ fn main() {
     {
         // How to Write a Custom derive Macro
         Pancakes::hello_macro();
+    }
+
+    {
+        // Attribute-like macros
+        hello_attr_like_macro();
     }
 }
