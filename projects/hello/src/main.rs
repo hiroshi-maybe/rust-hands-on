2a7e484d1387b3a3095388dc9774b86aef24a378
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-01-10 11:31:33 -0800

    Complete Ch 20-1 Building a Single-Threaded Web Server

--- projects/hello/src/main.rs
+++ projects/hello/src/main.rs
@@ -19,15 +19,32 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
+    let get = b"GET / HTTP/1.1\r\n";
 
     println!("Request: {}", String::from_utf8_lossy((&buffer[..])));
 
+    if buffer.starts_with(get) {
+        let response = response_from_file(200, "OK", "hello.html");
+        write(stream, &response);
+    } else {
+        println!("Unexpected request");
+        let response = response_from_file(404, "NOT FOUND", "404.html");
+        write(stream, &response);
+    }
+}
+
+fn response_from_file(status_code: u32, reason: &str, file_name: &str) -> String {
+    let contents = fs::read_to_string(file_name).unwrap();
+    format!(
+        "HTTP/1.1 {} {}\r\nContent-Length: {}\r\n\r\n{}",
+        status_code,
+        reason,
+        contents.len(),
+        contents
+    )
+}
+
+fn write(mut stream: TcpStream, response: &String) {
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
