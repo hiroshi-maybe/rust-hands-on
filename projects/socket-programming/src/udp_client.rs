2c60454f78afd775ebfe64c8215cbf8c8d26988a
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-01-24 20:58:36 -0800

    Add more logs

--- projects/socket-programming/src/udp_client.rs
+++ projects/socket-programming/src/udp_client.rs
@@ -5,12 +5,17 @@ pub fn communicate(address: &str) -> Result<(), failure::Error> {
     let socket = UdpSocket::bind("127.0.0.1:0")?;
     loop {
         let mut input = String::new();
+        println!("Waiting for next input...");
         io::stdin().read_line(&mut input)?;
+        // Default maximum size to be sent through UDP in Mac OS.
+        // https://stackoverflow.com/questions/22819214/udp-message-too-long
+        // let input = "a".to_string().repeat(9216);
 
         socket.send_to(input.as_bytes(), address)?;
         let mut buffer = [0u8; 1024];
-        socket.recv_from(&mut buffer).expect("Failed to receive");
-        print!(
+        let (size, src) = socket.recv_from(&mut buffer).expect("Failed to receive");
+        debug!("Received data from {} with size {}", src, size);
+        println!(
             "{}",
             str::from_utf8(&buffer).expect("Failed to convert to String")
         );
--- projects/socket-programming/src/udp_server.rs
+++ projects/socket-programming/src/udp_server.rs
@@ -5,9 +5,10 @@ pub fn serve(address: &str) -> Result<(), failure::Error> {
     let socket = UdpSocket::bind(address)?;
     loop {
         let mut buf = [0u8; 1024];
+        debug!("Waiting for next data...");
         let (size, src) = socket.recv_from(&mut buf)?;
-        debug!("Handling data from {}", src);
-        print!("{}", str::from_utf8(&buf[..size])?);
+        debug!("Handling data from {} with size {}", src, size);
+        println!("{}", str::from_utf8(&buf[..size])?);
         socket.send_to(&buf, src)?;
     }
 }
