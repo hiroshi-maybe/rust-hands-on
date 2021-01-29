b8f2d017f9627e02dda5570db94bc3e1837d02d8
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-01-29 09:55:30 -0800

    Complete packet capture implementation

--- projects/packet-capture/src/main.rs
+++ projects/packet-capture/src/main.rs
@@ -5,8 +5,15 @@ use pnet::packet::ethernet::{EthernetPacket, EtherTypes};
 use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::ipv6::Ipv6Packet;
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::udp::UdpPacket;
 use pnet::packet::Packet;
 use std::env;
+
+mod packets;
+use packets::GettableEndPoints;
+
+const WIDTH: usize = 20;
 
 fn main() {
     env::set_var("RUST_LOG", "debug");
@@ -57,10 +64,10 @@ fn ipv4_handler(ethernet: &EthernetPacket) {
         Some(packet) => {
             match packet.get_next_level_protocol() {
                 IpNextHeaderProtocols::Tcp => {
-                    //tcp_handler(&packet);
+                    tcp_handler(&packet);
                 },
                 IpNextHeaderProtocols::Udp => {
-                    //udp_handler(&packet);
+                    udp_handler(&packet);
                 },
                 _ => {
                     info!("Neither TCP nor UDP packet")
@@ -77,10 +84,10 @@ fn ipv6_handler(ethernet: &EthernetPacket) {
         Some(packet) => {
             match packet.get_next_header() {
                 IpNextHeaderProtocols::Tcp => {
-                    //tcp_handler(&packet);
+                    tcp_handler(&packet);
                 },
                 IpNextHeaderProtocols::Udp => {
-                    //udp_handler(&packet);
+                    udp_handler(&packet);
                 },
                 _ => {
                     info!("Neither TCP nor UDP packet")
@@ -93,4 +100,49 @@ fn ipv6_handler(ethernet: &EthernetPacket) {
     }
 
 }
-//fn tcp_handler(packet: &GettableEndPoints) {}
+fn tcp_handler(packet: &dyn GettableEndPoints) {
+    let tcp = TcpPacket::new(packet.get_payload());
+    if let Some(tcp) = tcp {
+        print_packet_info(packet, &tcp, "TCP");
+    }
+}
+
+fn udp_handler(packet: &dyn GettableEndPoints) {
+    let udp = UdpPacket::new(packet.get_payload());
+    if let Some(udp) = udp {
+        print_packet_info(packet, &udp, "UDP");
+    }
+}
+
+fn print_packet_info(l3: &dyn GettableEndPoints, l4: &dyn GettableEndPoints, proto: &str) {
+    println!(
+        "Captured a {} packet from {}|{} to {}|{}\n",
+        proto,
+        l3.get_source(),
+        l4.get_source(),
+        l3.get_destination(),
+        l4.get_destination()
+    );
+    let payload = l4.get_payload();
+    let len = payload.len();
+
+    for i in 0..len {
+        print!("{:<02X} ", payload[i]);
+        if i % WIDTH == WIDTH - 1 || i == len - 1 {
+            for _j in 0..WIDTH - 1 - (i % (WIDTH)) {
+                print!("     ");
+            }
+            print!("|   ");
+            for j in i - i % WIDTH..=i {
+                if payload[j].is_ascii_alphabetic() {
+                    print!("{}", payload[j] as char);
+                } else {
+                    print!(".");
+                }
+            }
+            println!();
+        }
+    }
+    println!("{}","=".repeat(WIDTH * 3));
+    println!();
+}
