d4a0125019f5b757181c13bee2be4bb8590d2d77
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-02-02 09:28:25 -0800

    Run cargo fmt

--- projects/port-scanner/src/main.rs
+++ projects/port-scanner/src/main.rs
@@ -1,11 +1,11 @@
 #[macro_use]
 extern crate log;
+use pnet::packet::tcp::TcpFlags;
 use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::net::Ipv4Addr;
 use std::process;
-use pnet::packet::tcp::TcpFlags;
 
 /// Usage:
 /// $ cargo run 127.0.0.1 sS
@@ -43,12 +43,13 @@ fn main() {
 
     let packet_info = {
         let contents = fs::read_to_string(".env").expect("File .env read error");
-        let map: HashMap<_, _> = contents.lines()
+        let map: HashMap<_, _> = contents
+            .lines()
             .filter_map(|line| {
                 let tokens = line.split("=").map(str::trim).collect::<Vec<_>>();
                 match tokens.len() {
                     2 => Some((tokens[0], tokens[1])),
-                    _ => None
+                    _ => None,
                 }
             })
             .collect();
@@ -57,17 +58,22 @@ fn main() {
             my_ipaddr: map["MY_IPADDR"].parse().expect("invalid ipaddr"),
             target_ipaddr: args[1].parse().expect("invalid target ipaddr"),
             my_port: map["MY_PORT"].parse().expect("invalid port number"),
-            maximum_port: map["MAXIMUM_PORT_NUM"].parse().expect("invalid maximum port num"),
+            maximum_port: map["MAXIMUM_PORT_NUM"]
+                .parse()
+                .expect("invalid maximum port num"),
             scan_type: match args[2].as_str() {
                 SYN_METHOD => ScanType::Syn,
                 FIN_METHOD => ScanType::Fin,
                 XMAX_METHOD => ScanType::Xmas,
                 NULL_METHOD => ScanType::Null,
                 _ => {
-                    error!("Undefined scan method, only accept [{}|{}|{}|{}].", SYN_METHOD, FIN_METHOD, XMAX_METHOD, NULL_METHOD);
+                    error!(
+                        "Undefined scan method, only accept [{}|{}|{}|{}].",
+                        SYN_METHOD, FIN_METHOD, XMAX_METHOD, NULL_METHOD
+                    );
                     process::exit(1);
                 }
-            }
+            },
         }
     };
 
