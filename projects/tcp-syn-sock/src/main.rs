7eca6073b5f541c664e11a79268eacb176947348
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-02-14 09:06:15 -0800

    Add a note that BSD Unix does not support raw socket for TCP

--- projects/tcp-syn-sock/src/main.rs
+++ projects/tcp-syn-sock/src/main.rs
@@ -1,15 +1,32 @@
 use pnet::packet::ip::IpNextHeaderProtocols;
-use pnet::packet::tcp::{self, MutableTcpPacket, TcpFlags, TcpPacket};
+use pnet::packet::tcp::{self, MutableTcpPacket, TcpFlags};
 use pnet::transport::{self, TransportChannelType, TransportProtocol};
-use std::net::{self, Ipv4Addr};
-use std::thread;
+use std::net;
 
 const TARGET_IPADDR: &str = "10.0.1.1";
 const TCP_SIZE: usize = 20;
 const SRC_PORT: u16 = 33333;
 const DEST_PORT: u16 = 47;
 
+/// I figured out that raw IP datagram is never passed in Mac OS (Free BSD).
+/// https://stackoverflow.com/questions/6878603/strange-raw-socket-on-mac-os-x
+/// https://sock-raw.org/papers/sock_raw
+
+
 fn main() {
+    send_and_recv_raw_ip_datagram();
+}
+
+fn send_and_recv_raw_ip_datagram() {
+    // https://stackoverflow.com/questions/6878603/strange-raw-socket-on-mac-os-x
+    // https://docs.rs/pnet_transport/0.22.0/src/pnet_transport/lib.rs.html#106
+    //
+    // pnet_sys::socket(AF_INET, SOCK_RAW, 6)
+    //  -> libc::socket(libc::AF_INET = 2, libc::SOCK_RAW = 3, 6)
+    //
+    // - AF_INET = 2 from https://github.com/rust-lang/libc/blob/2a2196d6dc2aec3ef0b0bcd11231db1f3b2ca04e/src/unix/bsd/apple/mod.rs#L2173
+    // - SOCK_RAW = 3 from https://github.com/rust-lang/libc/blob/2a2196d6dc2aec3ef0b0bcd11231db1f3b2ca04e/src/unix/bsd/apple/mod.rs#L2258
+    // `6` is a protocol number for TCP http://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
     let (mut ts, mut tr) = transport::transport_channel(
         1024,
         TransportChannelType::Layer4(TransportProtocol::Ipv4(IpNextHeaderProtocols::Tcp)),
@@ -17,21 +34,21 @@ fn main() {
     .expect("Failed to open channel.");
 
     let mut tcp_buffer = [0u8; TCP_SIZE];
-    let mut tcp_header = MutableTcpPacket::new(&mut tcp_buffer[..]).unwrap();
-    tcp_header.set_source(SRC_PORT);
-    tcp_header.set_data_offset(5);
-    tcp_header.set_flags(TcpFlags::SYN);
-    tcp_header.set_destination(DEST_PORT);
-    let target_ipaddr = TARGET_IPADDR.parse().expect("invalid ipaddr");
-    let checksum = tcp::ipv4_checksum(
-        &tcp_header.to_immutable(),
-        &"10.0.1.2".parse().expect("invalid ipaddr"),
-        &target_ipaddr
-    );
-    tcp_header.set_checksum(checksum);
-    let size = ts.send_to(tcp_header, net::IpAddr::V4(target_ipaddr))
-        .expect("failed to send a packet");
-    println!("Port {} sent with size {}", DEST_PORT, size);
+        let mut tcp_header = MutableTcpPacket::new(&mut tcp_buffer[..]).unwrap();
+        tcp_header.set_source(SRC_PORT);
+        tcp_header.set_data_offset(5);
+        tcp_header.set_flags(TcpFlags::SYN);
+        tcp_header.set_destination(DEST_PORT);
+        let target_ipaddr = TARGET_IPADDR.parse().expect("invalid ipaddr");
+        let checksum = tcp::ipv4_checksum(
+            &tcp_header.to_immutable(),
+            &"10.0.1.2".parse().expect("invalid ipaddr"),
+            &target_ipaddr
+        );
+        tcp_header.set_checksum(checksum);
+        let size = ts.send_to(tcp_header, net::IpAddr::V4(target_ipaddr))
+            .expect("failed to send a packet");
+        println!("Port {} sent with size {}", DEST_PORT, size);
 
     let mut packet_iter = transport::tcp_packet_iter(&mut tr);
     loop {
