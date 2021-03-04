583bde3ce3be6d6f41e4fa5aded1108a3b21c715
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-03-04 09:40:25 -0800

    Define a structure of webserver

--- projects/mio-webserver/src/main.rs
+++ projects/mio-webserver/src/main.rs
@@ -1,3 +1,24 @@
+use mio::tcp::{TcpListener, TcpStream};
+use std::collections::HashMap;
+
+struct WebServer {
+    listening_socket: TcpListener,
+    connections: HashMap<usize, TcpStream>,
+    next_connection_id: usize,
+}
+
+impl WebServer {
+    fn new(addr: &str) -> Result<Self, failure::Error> {
+        let address = addr.parse()?;
+        let listening_socket = TcpListener::bind(&address)?;
+        Ok(WebServer {
+            listening_socket,
+            connections: HashMap::new(),
+            next_connection_id: 1,
+        })
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
