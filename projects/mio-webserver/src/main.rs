ac6b3d4cde57b73213d1066b64d7bc25bdcd8858
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-03-31 09:41:42 -0700

    Add response maker

--- projects/mio-webserver/src/main.rs
+++ projects/mio-webserver/src/main.rs
@@ -1,10 +1,14 @@
 use mio::tcp::{TcpListener, TcpStream};
 use mio::{Event, Events, Poll, PollOpt, Ready, Token};
+use regex::Regex;
 use std::collections::HashMap;
-use std::io::{Read, Write};
+use std::env;
+use std::fs::File;
+use std::io::{BufReader, Read, Write};
 #[macro_use]
 extern crate log;
 
+const WEBROOT: &str = "/webroot";
 const SERVER: Token = Token(0);
 
 struct WebServer {
@@ -149,7 +153,62 @@ impl WebServer {
 }
 
 fn make_response(_buffer: &[u8]) -> Result<Vec<u8>, failure::Error> {
-    Ok(vec![])
+    let http_pattern = Regex::new(r"(.*) (.*) HTTP/1.([0-1])\r\n.*")?;
+    let captures = match http_pattern.captures(str::from_utf8(buffer)?) {
+        Some(cap) => cap,
+        None => {
+            return create_msg_from_code(400, None);
+        }
+    };
+
+    let method = captures[1].to_string();
+    let path = format!(
+        "{}{}{}",
+        env::current_dir()?.display(),
+        WEBROOT,
+        &captures[2]
+    );
+    let _version = captures[3].to_string();
+
+    if method != "GET" {
+        return create_msg_from_code(501, None);
+    }
+
+    let file = match File::open(path) {
+        Ok(file) => file,
+        Err(_) => {
+            return create_msg_from_code(404, None);
+        }
+    };
+    let mut reader = BufReader::new(file);
+    let mut buf = Vec::new();
+    reader.read_to_end(&mut buf)?;
+    create_msg_from_code(200, Some(buf))
+}
+
+const OK_200: &str = "200 OK";
+const ERROR_400: &str = "400 Bad Request";
+const ERROR_404: &str = "404 Not Found";
+const ERROR_501: &str = "501 Not Implemented";
+
+fn create_msg_from_code(status_code: u16, msg: Option<Vec<u8>>) -> Result<Vec<u8>, failure::Error> {
+    match status_code {
+        200 => {
+            let header = create_res_msg(OK_200);
+            Ok(msg.map_or_else(|| header, |body| [&header[..], &body[..]].concat()))
+        },
+        400 => Ok(create_res_msg(ERROR_400)),
+        404 => Ok(create_res_msg(ERROR_404)),
+        501 => Ok(create_res_msg(ERROR_501)),
+        _ => Err(failure::err_msg("Unexpected status code."))
+    }
+}
+
+fn create_res_msg(msg: &str) -> Vec<u8> {
+    format!("HTTP/1.0 {}\r\n\
+             Server: mio webserver\r\n\r\n", msg)
+             .to_string()
+             .into_bytes()
 }
 
 fn main() {
