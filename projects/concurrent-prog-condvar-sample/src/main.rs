776708ac5ce593fcaf730208db2450dee0214ea2
Hiroshi Kori <hiroshi.maybe@gmail.com>
2022-09-22 21:48:53 +0900

    add Condition variable sample for Concurrent Programming 3.8.2

---
+++ projects/concurrent-prog-condvar-sample/src/main.rs
@@ -0,0 +1,46 @@
+use std::{
+    sync::{Arc, Condvar, Mutex},
+    thread,
+};
+
+/// https://www.oreilly.co.jp/books/9784873119595/
+/// Ch 3.8.2
+
+fn child(id: u64, p: Arc<(Mutex<bool>, Condvar)>) {
+    let &(ref lock, ref cvar) = &*p;
+
+    let mut started = lock.lock().unwrap();
+    while !*started {
+        println!("child{} is waiting..", id);
+        started = cvar.wait(started).unwrap();
+        println!("child{} waited", id);
+    }
+
+    // cvar.wait_while(started, |s| !*s).unwrap();
+    println!("child{} done", id);
+}
+
+fn parent(p: Arc<(Mutex<bool>, Condvar)>) {
+    let &(ref lock, ref cvar) = &*p;
+
+    let mut started = lock.lock().unwrap();
+    *started = true;
+    cvar.notify_all();
+    // cvar.notify_one();
+
+    println!("parent done");
+}
+
+fn main() {
+    let pair0 = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair1 = pair0.clone();
+    let pair2 = pair0.clone();
+
+    let c0 = thread::spawn(move || child(0, pair0));
+    let c1 = thread::spawn(move || child(1, pair1));
+    let p = thread::spawn(move || parent(pair2));
+
+    c0.join().unwrap();
+    c1.join().unwrap();
+    p.join().unwrap();
+}
