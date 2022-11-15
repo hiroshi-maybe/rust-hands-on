471f086ea1305a09a4cd7e8b7aa3e1efeab25189
Hiroshi Kori <hiroshi.maybe@gmail.com>
2022-11-14 22:09:34 -0800

    use async/await to call a future

--- projects/concurrent-prog-coroutine-like-hello-world/src/main.rs
+++ projects/concurrent-prog-coroutine-like-hello-world/src/main.rs
@@ -85,7 +85,10 @@ impl Spawner {
 
 fn main() {
     let executor = Executor::new();
-    executor.get_spawner().spawn(Hello::new());
+    executor.get_spawner().spawn(async {
+        let h = Hello::new();
+        h.await;
+    });
     executor.run();
 }
 
