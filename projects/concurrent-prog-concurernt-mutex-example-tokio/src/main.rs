9dff2eb10d9422c1c015ff3f323c99ce2a25b8af
Hiroshi Kori <hiroshi.maybe@gmail.com>
2022-11-26 17:28:14 -0800

    add mutex example without overlapping awaits for Concurent Programming 5.4

---
+++ projects/concurrent-prog-concurernt-mutex-example-tokio/src/main.rs
@@ -0,0 +1,33 @@
+use std::sync::{Arc, Mutex};
+
+const NUM_TASKS: usize = 4;
+const NUM_LOOP: usize = 100_000;
+
+#[tokio::main]
+async fn main() -> Result<(), tokio::task::JoinError> {
+    let val = Arc::new(Mutex::new(0));
+    let mut ts = vec![];
+
+    for _ in 0..NUM_TASKS {
+        let n = val.clone();
+        let t = tokio::spawn(async move {
+            for _ in 0..NUM_LOOP {
+                let mut n0 = n.lock().unwrap();
+                *n0 += 1;
+            }
+        });
+        ts.push(t);
+    }
+
+    for t in ts {
+        t.await?;
+    }
+
+    println!(
+        "COUNT = {} (expected = {})",
+        *val.lock().unwrap(),
+        NUM_LOOP * NUM_TASKS
+    );
+
+    Ok(())
+}
