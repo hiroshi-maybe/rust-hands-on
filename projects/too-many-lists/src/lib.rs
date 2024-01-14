51f31386591b3bf8b32a0fb283d59acc90cd8652
Hiroshi Kori <hiroshi.maybe@gmail.com>
2024-01-13 22:38:27 -0800

    implement basics of a bad safe deque

--- projects/too-many-lists/src/lib.rs
+++ projects/too-many-lists/src/lib.rs
@@ -1,3 +1,7 @@
+/// Box to avoid infinitely recursive type
 pub mod first;
+/// RefCell to achieve interior mutability for doubly-linked list
+pub mod fourth;
 pub mod second;
+/// Rc to share ownerships
 pub mod third;
