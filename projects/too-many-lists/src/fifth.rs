231455f47b39604a355973d00951284f3c8a8945
Hiroshi Kori <hiroshi.maybe@gmail.com>
2024-01-17 21:06:31 -0800

    implement push/pop for unsafe queue

--- projects/too-many-lists/src/fifth.rs
+++ projects/too-many-lists/src/fifth.rs
@@ -21,11 +21,32 @@ impl<T> List<T> {
     }
 
     pub fn push(&mut self, elem: T) {
-        unimplemented!()
+        let mut new_tail = Box::new(Node {
+            elem: elem,
+            next: None,
+        });
+        let raw_tail: *mut _ = &mut *new_tail;
+
+        if !self.tail.is_null() {
+            unsafe {
+                (*self.tail).next = Some(new_tail);
+            }
+        } else {
+            self.head = Some(new_tail);
+        }
+
+        self.tail = raw_tail;
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        unimplemented!()
+        self.head.take().map(|old_head| {
+            self.head = old_head.next;
+            if self.head.is_none() {
+                self.tail = ptr::null_mut();
+            }
+
+            old_head.elem
+        })
     }
 }
 
@@ -33,5 +54,40 @@ impl<T> List<T> {
 mod test {
     use super::List;
     #[test]
-    fn basics() {}
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        // Check the exhaustion case fixed the pointer right
+        list.push(6);
+        list.push(7);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(7));
+        assert_eq!(list.pop(), None);
+    }
 }
