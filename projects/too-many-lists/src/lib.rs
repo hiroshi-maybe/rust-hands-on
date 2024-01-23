5f84553326b87ee38740e7c63b50bd50848efe15
Hiroshi Kori <hiroshi.maybe@gmail.com>
2024-01-22 22:36:58 -0800

    make a layout of production unsafe deque

--- projects/too-many-lists/src/lib.rs
+++ projects/too-many-lists/src/lib.rs
@@ -5,5 +5,7 @@ pub mod first;
 /// RefCell to achieve interior mutability for doubly-linked list
 pub mod fourth;
 pub mod second;
+/// Doubly linked list
+pub mod sixth;
 /// Rc to share ownerships
 pub mod third;
